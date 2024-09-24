07993d290310a36e22408d018206c74e3f58f389
alschlee <1daysixmeal@gmail.com>
2024-09-24 11:22:31 +0900

    environment 이동으로 인한 임시 push

--- cliapp/cli.py
+++ cliapp/cli.py
@@ -1,13 +1,12 @@
-# 매개변수로 전달될 값을 이용해서 http://192/168.38.114 요청을 전달하고 응답을 출력하는 프로그램
-# python cli.py ls 형식으로 실행하면 http://192/168.38.114/container/ls 요청을 전달하고 응답 출력
+# 매개변수로 전달될 값을 이용해서 http://106.101.9.129 요청을 전달하고 응답을 출력하는 프로그램
+# python cli.py ls 형식으로 실행하면 http://106.101.9.129/container/ls 요청을 전달하고 응답 출력
 
-import requests
 import requests
 import sys
 
 
 cmd = sys.argv[1]
-url = f"http://192.168.38.114/container/{cmd}"
+url = f"http://172.20.10.2/container/{cmd}"
 response = requests.get(url)
 response = response.text[1:-1].replace("\\\"", "\"")
 
--- cliapp/main.py
+++ cliapp/main.py
@@ -2,13 +2,22 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# 실행 상태의 도커 컨테이너 조회
 @app.get("/container/ls")
 # docker container ls 명령어의 실행 결과를 반환
 def container_ls():
     import subprocess
-    output = subprocess.check_output("docker container ls", shell = True)
+    output = subprocess.check_output("docker container ls", shell=True)
     return output.decode("utf-8")
 
+# 실행 상태의 도커 컨테이너 목록을 JSON 형식으로 조회
+@app.get("/container/ps")
+def container_ls():
+    import subprocess
+    output = subprocess.check_output("docker container ls --format json", shell=True)
+    return output.decode("utf-8")
+
+# nginx 컨테이너 생성 후 컨테이너 정보 반환
 @app.get("/container/run")
 def container_run():
     import subprocess
@@ -17,6 +26,8 @@ def container_run():
     output = subprocess.check_output(f"docker container ls  -f id={output}", shell=True)
     return output.decode("utf-8")
 
+# 80 포트로 실행
+# host="0.0.0.0" ~> 모든 IP 주소로 부터의 요청 처리
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host = "127.0.0.1", port = 80, log_level = "debug", reload = True)
+    uvicorn.run("main:app", host="0.0.0.0", port=80, log_level="debug", reload=True)
